a97ca265198bfdf570fb70a15afdd2700c93a6f8
BugBounty89 <akashwaichal34@gmail.com>
2020-09-22 22:56:24 +0530

    user activation link added

--- admin-server.js
+++ admin-server.js
@@ -2,11 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 // routes
-const routerAdmin       = require('./admin/routes/admin')
-const routerUser        = require('./admin/routes/user')
-const routerOrganizer   = require('./admin/routes/organizer')
-const routerEvent       = require('./admin/routes/event')
-const routerFeedback    = require('./admin/routes/feedback')
+const routerAdmin = require('./admin/routes/admin')
+const routerUser = require('./admin/routes/user')
+const routerOrganizer = require('./admin/routes/organizer')
+
 
 const app = express()
 
@@ -17,8 +16,7 @@ app.use(bodyParser.json())
 app.use('/admin', routerAdmin)
 app.use('/user', routerUser)
 app.use('/organizer', routerOrganizer)
-app.use('/event', routerEvent)
-app.use('/feedback', routerFeedback)
+
 
 
 app.listen(3000, '0.0.0.0', () => {
--- organizer-user-server.js
+++ organizer-user-server.js
@@ -3,7 +3,10 @@ const bodyParser = require('body-parser')
 
 // routes
 const routerorganizer_user = require('./organizer_user/routes/organizer')
+
 const routerorganizer_user1 = require('./organizer_user/routes/user')
+
+
 
 
 const app = express()
@@ -14,6 +17,7 @@ app.use(bodyParser.json())
 // add routes to the application
 app.use('/organizer_user/org', routerorganizer_user)
 app.use('/organizer_user/user', routerorganizer_user1)
+
 
 
 app.listen(4000, '0.0.0.0', () => {
