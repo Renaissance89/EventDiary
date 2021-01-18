650ed53d4fa3697aac77c0a1f4f85fca5e60b7c5
Akash Waichal <akashwaichal34@gmail.com>
2021-01-18 17:14:51 +0530

    user paymrnt html added

--- frontend/angular/user/src/app/user/event/event.module.ts
+++ frontend/angular/user/src/app/user/event/event.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { EventRoutingModule } from './event-routing.module';
 import { EventListComponent } from './event-list/event-list.component';
 import { CartComponent } from './cart/cart.component';
+import { PaymentComponent } from './payment/payment.component';
 
 
 @NgModule({
-  declarations: [EventListComponent, CartComponent],
+  declarations: [EventListComponent, CartComponent, PaymentComponent],
   imports: [
     CommonModule,
     EventRoutingModule
